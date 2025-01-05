5f0f64314a8de6c9b6dc114d08e1bc32c3220416
Nero <david.zeballos@alumnos.ucn.cl>
2025-01-05 16:13:59 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IDWM_TallerMovil',
+  webDir: 'www'
+};
+
+export default config;
